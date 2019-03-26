83e68fd5ebee41b231a9fce804f7c46346af3fef
Yuliusfigoo <yulius.2293@students.amikom.ac.id>
2019-03-26 13:45:49 +0700

    wkwk

---
+++ mahasiswa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //mengeset nilai properties objek saya dan kamu
+            saya.Nim = "12345";
+            saya.Nama = "Yulius";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "12344";
+            kamu.Nama = "Tegar";
+            kamu.Ipk = 3.70f;
+
+            // memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
